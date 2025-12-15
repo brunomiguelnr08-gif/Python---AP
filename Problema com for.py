036b42b30986b47465ab19a073deea3d1030c088
Rocha <brunomiguel.nr08@gmail.com>
2025-12-15 09:20:12 +0000

    Create Problema com for.py

---
+++ Problema com for.py
@@ -0,0 +1,13 @@
+LInf=int(input("Insira o valor do limite inferior: "))
+LSup=int(input("Insira o valor do limite superior: "))
+
+if(LSup>LInf):
+    for i in range(LInf,LSup):
+        print(i)
+else:
+    print("Os limites, indicados para o intervalo, estão errados.")
+
+    
+
+    
+
